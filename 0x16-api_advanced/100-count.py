77911a919ca8d15e11c90e524afbb45ff9dd93aa
HammedAbdulazeez <hammedabdulazeez@outlook.com>
2023-06-21 22:32:36 +0100

    api adv

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,44 +1,44 @@
 import requests
-import re
-from collections import Counter
 
-
-def count_words(subreddit, word_list, after=None, counts=None):
-    if counts is None:
-        counts = Counter()
-
-    # Base case: If there are no more words to search or the subreddit is invalid, print the results
-    if not word_list or subreddit is None:
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_counts:
-            print(f"{word.lower()}: {count}")
+def count_words(subreddit, word_list, after=None, word_count={}):
+    '''Recursively counts the occurrences of given keywords in the titles
+    of hot articles in a given subreddit.
+    '''
+    if not word_list:
         return
 
-    # Fetch the Reddit API data
-    headers = {"User-Agent": "Recursive Reddit API Client"}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    params = {"after": after, "limit": 100}
-    response = requests.get(url, headers=headers, params=params)
+    if after is None:
+        word_count.clear()
 
-    # Check if the subreddit is valid and the API request was successful
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+    }
+    params = {
+        'limit': 100,
+        'after': after
+    }
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+
     if response.status_code == 200:
-        data = response.json()
-        posts = data["data"]["children"]
-
-        # Extract the titles and count the occurrences of keywords
+        data = response.json().get('data')
+        posts = data.get('children')
         for post in posts:
-            title = post["data"]["title"]
-            lowercase_title = title.lower()
-            word_count = Counter(re.findall(r"\b\w+\b", lowercase_title))
+            title = post.get('data').get('title').lower()
             for word in word_list:
-                if word in word_count:
-                    counts[word] += word_count[word]
-
-        # Recursive call with the next page (if available)
-        next_after = data["data"]["after"]
-        count_words(subreddit, word_list, after=next_after, counts=counts)
-
-
-# Example usage
-count_words("python", ["Python", "programming", "API"])
+                count = title.count(' ' + word.lower() + ' ')
+                if word not in word_count:
+                    word_count[word] = count
+                else:
+                    word_count[word] += count
+        after = data.get('after')
+        if after is not None:
+            count_words(subreddit, word_list, after, word_count)
+        else:
+            sorted_words = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_words:
+                if count > 0:
+                    print('{}: {}'.format(word.lower(), count))
+    else:
+        return
 
